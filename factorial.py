69d208b25d8746d9d7ad53f7a408ac4dfe5d0649
Mitesh Chaudhari <miteshchaudhari4466@gmail.com>
2024-02-14 22:21:45 +0530

    update

--- factorial.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-n = int(input("Enter n: "))
-result = factorial(n)
-print("Factorial of", n, "is", result)
--- gratof2.py
+++
@@ -1,6 +0,0 @@
-a = 30
-b = 20
-if(a>b):
-        print(a)
-else:
-        print(b)
