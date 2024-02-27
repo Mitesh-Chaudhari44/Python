0b04096819958edc749a6b2b7573d3860fc473b0
Mitesh Chaudhari <miteshchaudhari4466@gmail.com>
2024-02-27 19:31:34 +0530

    updated

---
+++ result.py
@@ -0,0 +1,4 @@
+for i in range(0,5):
+    marks=(int(input(f"Enter the marks of student {i+1}")))
+if marks>=40:
+    print("pass")
---
+++ star pattern/right_tri.py
@@ -0,0 +1,7 @@
+n=int(input("Enter n:"))
+for i in range(n):
+    for j in range(1,n-1):
+        print(" ",end="")
+    for k in range(0,i+1):
+        print("*",end="")
+    print()
