2e18159b25601557df8dc57beb0e7c986a86ae8c
Mitesh-Chaudhari44 <159791285+Mitesh-Chaudhari44@users.noreply.github.com>
2024-04-16 11:18:53 +0530

    Create Check_upper.py

---
+++ Check_upper.py
@@ -0,0 +1,9 @@
+def is_uppercase(string):
+    return string.isupper()
+
+# Example usage:
+input_string = "HELLO"
+if is_uppercase(input_string):
+    print("The string is in uppercase.")
+else:
+    print("The string is not in uppercase.")
